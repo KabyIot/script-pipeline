53d4606368cf16911402b6af0501d09905858577
KabyIot <121157316+KabyIot@users.noreply.github.com>
2024-02-23 02:29:13 +0100

    Create worker.sh

---
+++ Kubernetes/worker.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Run as Root
+if [[ "$(id -u)" -ne 0 ]]; then
+    echo "Det här skriptet måste köras som root."
+    exit 1
+fi
+# Update and upgrade package
+apt-get update && apt-get upgrade -y
+# Install necessary package
+apt-get install -y docker.io docker-compose git openssl
+# Disable swap & add kernel settings
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+# Add kernel settings & Enable IP tables (CNI Prerequisites)
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+modprobe overlay
+modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sysctl --system
+# Install containerd run time dependencies
+apt-get update -y
+apt-get install ca-certificates curl gnupg lsb-release -y
+# Add Docker’s official GPG key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# Set up the repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Install containerd
+apt-get update -y
+apt-get install containerd.io -y
+# Generate default configuration file for containerd
+containerd config default > /etc/containerd/config.toml
+# Update configure cgroup as systemd for containerd
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+# Restart and enable containerd service
+systemctl restart containerd
+systemctl enable containerd
+# Installing kubeadm, kubelet and kubectl
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
+# Download the Google Cloud public signing key
+curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+# Add the Kubernetes apt repository
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# Update apt package index, install kubelet, kubeadm and kubectl, and pin their version
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+# Enable and start kubelet service
+systemctl daemon-reload
+systemctl start kubelet
+systemctl enable kubelet.service
